58571f072663776d03c9ccd3438b454fc877ac51
richardjlyon <richard@richardlyon.net>
2018-03-06 15:54:42 +0000

    add UV Index

--- src/TempHumidityRHT03.ino
+++ src/TempHumidityRHT03.ino
@@ -12,8 +12,8 @@
  #include "authenticate.h" // InfluxDB credentials
  #include "InfluxDB.h"
 
- #define DELAY_BEFORE_REBOOT (10 * 1000)
- #define SAMPLE_RATE (30*1000) // Time between samples (miliseconds)
+ #define DELAY_BEFORE_REBOOT (20 * 1000)
+ #define SAMPLE_RATE (1000 * 60 * 60 * 24 / 500) // 500 requests per day (OpenUV limit)
 
  #include <SparkFunRHT03.h>
 
@@ -22,8 +22,11 @@
 
  double temperature;
  double temperature_outside;
+ double temperature_outside_old;
  double humidity;
- String str_outside_temperature = String(-273);
+ double uv_index;
+ String str_outside_temperature = String(-999);
+ String str_uv_index = String(-1);
 
  bool resetFlag = false;
  unsigned int rebootDelayMillis = DELAY_BEFORE_REBOOT;
@@ -46,6 +49,9 @@
      // Subscribe to 'outside_temperature' webhook
      Particle.subscribe("hook-response/outside_temperature", didGetOutsideTemperature, MY_DEVICES);
 
+     // Subscribe to 'uv_index' webhook
+     Particle.subscribe("hook-response/uv_index", didGetUVIndex, MY_DEVICES);
+
      // Publish the reset function to Particle
      Particle.function("reset", cloudResetFunction);
 
@@ -67,18 +73,20 @@
          temperature = rht.tempC();
          humidity = rht.humidity();
 
-         // trigger webhook 'outaide_temperature'
+         // trigger webhook 'outside_temperature'
          Particle.publish("outside_temperature", str_outside_temperature, PRIVATE);
+
+         // trigger webhook 'uv_index'
+         Particle.publish("uv_index", str_uv_index, PRIVATE);
 
          // send to webhook -> influxDB
          Particle.publish("temperature", String(temperature), PRIVATE);
-
-         Particle.publish("t outside", String(temperature_outside), PRIVATE);
 
          // send to ubidots
          idb.add("temperature", temperature);
          idb.add("humidity", humidity);
          idb.add("outside_temperature", temperature_outside);
+         idb.add("uv_index", uv_index);
          idb.sendAll();
      }
 
@@ -89,12 +97,21 @@
    // Handle the 'outside_temperature' response
    String temperatureReturn = String(data);
    double temp = atof(temperatureReturn) - 273.15;
-   if (temp > -30.0)
+   if (temp > -20.0)
    {
      temperature_outside = round(temp*10)/10;
    }
  }
 
+ void didGetUVIndex(const char *event, const char *data) {
+   // Handle UV Index response
+   String uvindex_return = String(data);
+   double temp = atof(uvindex_return);
+   if (temp >= 0)
+   {
+     uv_index = temp;
+   }
+}
 
  int cloudResetFunction(String command)
  {

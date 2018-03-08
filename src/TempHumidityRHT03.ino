2a1e7a6539012896d4a16028a2915fb35c2bf9d0
richardjlyon <richard@richardlyon.net>
2018-03-08 06:59:19 +0000

    Get weather data from darksky.net

--- src/TempHumidityRHT03.ino
+++ src/TempHumidityRHT03.ino
@@ -1,7 +1,7 @@
 /*
  * Project TempHumidityRHT03
  * Description: Temperature and Humidity monitor (RHT03 sensor)
- * Retrieves outside temperature from 'openweathermap'
+ * Retrieves outside temperature from darksky.net
  * Author: Richard Lyon
  * Date: 27 December 2017
  * Version: 0.1
@@ -9,109 +9,95 @@
 * Device: kingswood_one
  */
 
- #include "authenticate.h" // InfluxDB credentials
- #include "InfluxDB.h"
+#include "authenticate.h" // InfluxDB credentials
+#include "InfluxDB.h"
 
- #define DELAY_BEFORE_REBOOT (20 * 1000)
- #define SAMPLE_RATE (1000 * 60 * 60 * 24 / 500) // 500 requests per day (OpenUV limit)
+#define DELAY_BEFORE_REBOOT (20 * 1000)
+#define SAMPLE_RATE (1000 * 60 * 60 * 24 / 1000) // 1000 requests per day (DarkSky limit)
 
- #include <SparkFunRHT03.h>
+#include <SparkFunRHT03.h>
 
- InfluxDB idb = InfluxDB(USERNAME, PASSWORD);
- RHT03 rht; // This creates a RTH03 object, which we'll use to interact with the sensor
+InfluxDB idb = InfluxDB(USERNAME, PASSWORD);
+RHT03 rht; // This creates a RTH03 object, which we'll use to interact with the sensor
 
- double temperature;
- double temperature_outside;
- double temperature_outside_old;
- double humidity;
- double uv_index;
- String str_outside_temperature = String(-999);
- String str_uv_index = String(-1);
+// RHT03 sensor data
+double temperature;
+double humidity;
 
- bool resetFlag = false;
- unsigned int rebootDelayMillis = DELAY_BEFORE_REBOOT;
- unsigned long rebootSync = millis();
- int cloudResetFunction(String command);
+// Dark Sky Data
+double cloud_cover;
+double humidity_outside;
+double temperature_outside;
+double uvindex_corrected;
+String data = String(10);
 
- void setup() {
-     rht.begin(D2); // Initialize an RHT03 sensor, with the data pin connected to D2.
+bool resetFlag = false;
+unsigned int rebootDelayMillis = DELAY_BEFORE_REBOOT;
+unsigned long rebootSync = millis();
+int cloudResetFunction(String command);
 
-     // Publish IP address to Particle
-     IPAddress myIP = WiFi.localIP();
-     Particle.publish("IP Address", String(myIP));
+void setup() {
+   rht.begin(D2); // Initialize an RHT03 sensor, with the data pin connected to D2.
 
-     // Publish variables to Particle
-     Particle.variable("temperature", &temperature, DOUBLE);
-     Particle.variable("humidity", &humidity, DOUBLE);
-     Particle.variable("t outside", &temperature_outside, DOUBLE);
+   // Publish IP address to Particle
+   IPAddress myIP = WiFi.localIP();
+   Particle.publish("IP Address", String(myIP));
 
+   // Subscribe to 'dark_sky' webhook
+   Particle.subscribe("hook-response/dark_sky", didGetDarkSkyData, MY_DEVICES);
 
-     // Subscribe to 'outside_temperature' webhook
-     Particle.subscribe("hook-response/outside_temperature", didGetOutsideTemperature, MY_DEVICES);
+   // Publish the reset function to Particle
+   Particle.function("reset", cloudResetFunction);
 
-     // Subscribe to 'uv_index' webhook
-     Particle.subscribe("hook-response/uv_index", didGetUVIndex, MY_DEVICES);
-
-     // Publish the reset function to Particle
-     Particle.function("reset", cloudResetFunction);
-
-     // Set device name on InfluxDB
-     idb.setDeviceName("kingswood_one");
- }
-
- void loop() {
-     // Handle cloud reset logic: requires a delay to prevent race conditions
-     if ((resetFlag) && (millis() - rebootSync >=  rebootDelayMillis)) {
-         Particle.publish("Remote Reset Initiated");
-         System.reset();
-     }
-
-     int updateRet = rht.update();
-
-     if (updateRet == 1)
-     {
-         temperature = rht.tempC();
-         humidity = rht.humidity();
-
-         // trigger webhook 'outside_temperature'
-         Particle.publish("outside_temperature", str_outside_temperature, PRIVATE);
-
-         // trigger webhook 'uv_index'
-         Particle.publish("uv_index", str_uv_index, PRIVATE);
-
-         // send to webhook -> influxDB
-         Particle.publish("temperature", String(temperature), PRIVATE);
-
-         // send to ubidots
-         idb.add("temperature", temperature);
-         idb.add("humidity", humidity);
-         idb.add("outside_temperature", temperature_outside);
-         idb.add("uv_index", uv_index);
-         idb.sendAll();
-     }
-
-     delay(SAMPLE_RATE);
- }
-
- void didGetOutsideTemperature(const char *event, const char *data) {
-   // Handle the 'outside_temperature' response
-   String temperatureReturn = String(data);
-   double temp = atof(temperatureReturn) - 273.15;
-   if (temp > -20.0)
-   {
-     temperature_outside = round(temp*10)/10;
-   }
- }
-
- void didGetUVIndex(const char *event, const char *data) {
-   // Handle UV Index response
-   String uvindex_return = String(data);
-   double temp = atof(uvindex_return);
-   if (temp >= 0)
-   {
-     uv_index = temp;
-   }
+   // Set device name on InfluxDB
+   idb.setDeviceName("kingswood_one");
 }
+
+void loop() {
+   // Handle cloud reset logic: requires a delay to prevent race conditions
+   if ((resetFlag) && (millis() - rebootSync >=  rebootDelayMillis)) {
+       Particle.publish("Remote Reset Initiated");
+       System.reset();
+   }
+
+   int updateRet = rht.update();
+
+   if (updateRet == 1)
+   {
+       // get sensor data
+       temperature = rht.tempC();
+       humidity = rht.humidity();
+
+       // trigger webhook 'didGetDarkSkyData'
+       Particle.publish("dark_sky", data, PRIVATE);
+
+       // update influxDB
+       idb.add("temperature", temperature);
+       idb.add("humidity", humidity);
+       idb.add("humidity_outside", humidity_outside);
+       idb.add("outside_temperature", temperature_outside);
+       idb.add("uv_index", uvindex_corrected);
+       idb.sendAll();
+   }
+
+   delay(SAMPLE_RATE);
+}
+
+
+ void didGetDarkSkyData(const char *event, const char *data) {
+     // format: cloudCover~humidity~temperature-uvIndex
+     String str = String(data);
+     char strBuffer[125] = "";
+     str.toCharArray(strBuffer, 125);
+
+     cloud_cover = atof(strtok(strBuffer, "~"));
+     humidity_outside = atof(strtok(NULL, "~"));
+     temperature_outside = (atof(strtok(NULL, "~")) - 32.0) * 5.0/9.0;
+     // correct UV Index for cloud cover
+     double transmission = cloud_cover * -0.69 + 1;
+     uvindex_corrected = atof(strtok(NULL, "~")) * transmission;
+ }
+
 
  int cloudResetFunction(String command)
  {

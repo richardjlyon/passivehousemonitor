9308ede9a161a2000c3840ce46eaffe5dbfac5a4
richardjlyon <richard@richardlyon.net>
2018-03-10 08:17:07 +0000

    tidy

--- src/TempHumidityRHT03.ino
+++ src/TempHumidityRHT03.ino
@@ -28,6 +28,8 @@ double humidity;
 double cloud_cover;
 double humidity_outside;
 double temperature_outside;
+double uvindex = 0;
+double transmission = 0;
 double uvindex_corrected;
 String data = String(10);
 
@@ -43,8 +45,15 @@ void setup() {
    IPAddress myIP = WiFi.localIP();
    Particle.publish("IP Address", String(myIP));
 
-   // Subscribe to 'dark_sky' webhook
+   // Declare Particle variables
+   Particle.variable("cloud_cover", cloud_cover);
+   Particle.variable("transmission", transmission);
+   Particle.variable("UV_index", uvindex);
+   Particle.variable("UV_index_cor", uvindex_corrected);
+
+   // Subscribe to webhooks
    Particle.subscribe("hook-response/dark_sky", didGetDarkSkyData, MY_DEVICES);
+   Particle.subscribe("hook-response/uv_index", didGetOpenUVData, MY_DEVICES);
 
    // Publish the reset function to Particle
    Particle.function("reset", cloudResetFunction);
@@ -68,16 +77,30 @@ void loop() {
        temperature = rht.tempC();
        humidity = rht.humidity();
 
-       // trigger webhook 'didGetDarkSkyData'
+       // trigger webhooks
        Particle.publish("dark_sky", data, PRIVATE);
+       Particle.publish("uv_index", data, PRIVATE);
+
+       // calculate corrected UV Index
+       transmission = (cloud_cover * -0.69) + 1.0;
+       uvindex_corrected = uvindex * transmission;
 
        // update influxDB
        idb.add("temperature", temperature);
        idb.add("humidity", humidity);
        idb.add("humidity_outside", humidity_outside);
        idb.add("outside_temperature", temperature_outside);
-       idb.add("uv_index", uvindex_corrected);
+       idb.add("cloud_cover", cloud_cover);
+       idb.add("uv_index", uvindex);
+       idb.add("uv_index_corrected", uvindex_corrected);
        idb.sendAll();
+
+       // Update console
+      //  Particle.publish("Updated InfluxDB");
+      //  Particle.publish("outside_temperature", String(temperature_outside));
+      //  Particle.publish("cloud_cover", String(cloud_cover));
+      //  Particle.publish("uvindex", String(uvindex));
+
    }
 
    delay(SAMPLE_RATE);
@@ -85,19 +108,20 @@ void loop() {
 
 
  void didGetDarkSkyData(const char *event, const char *data) {
-     // format: cloudCover~humidity~temperature-uvIndex
+     // format: cloudCover~humidity~temperature
      String str = String(data);
      char strBuffer[125] = "";
      str.toCharArray(strBuffer, 125);
 
      cloud_cover = atof(strtok(strBuffer, "~"));
      humidity_outside = atof(strtok(NULL, "~"));
-     temperature_outside = (atof(strtok(NULL, "~")) - 32.0) * 5.0/9.0;
-     // correct UV Index for cloud cover
-     double transmission = cloud_cover * -0.69 + 1;
-     uvindex_corrected = atof(strtok(NULL, "~")) * transmission;
+     temperature_outside = atof(strtok(NULL, "~"));
  }
 
+
+ void didGetOpenUVData(const char *event, const char *data) {
+   uvindex = atof(data);
+ }
 
  int cloudResetFunction(String command)
  {
